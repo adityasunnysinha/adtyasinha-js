7a35471c32b8f4fed1d31c4c10c9934c2d8c9107
Aditya Sinha <99471410+adityasunnysinha@users.noreply.github.com>
2024-07-24 19:31:37 +0000

    some changes

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -3,5 +3,4 @@ let accountPassword="123456789"
 var accountCity="bengaluru"
 console.log(accountCity);
 console.table([accountCity,accountEmail,accountPassword]);
-console.table([accountCity,accountEmail,accountPassword]);
-console.table([accountCity,accountEmail,accountPassword]);
+
