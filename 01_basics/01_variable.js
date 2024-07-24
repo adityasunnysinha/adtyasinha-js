8a4b46ce21a9861265a4fe875835fc0031a486c3
Aditya Sinha <99471410+adityasunnysinha@users.noreply.github.com>
2024-07-24 18:13:28 +0000

    first commit

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,5 @@
+const accountEmail="ram@gmail.com"
+let accountPassword="123456789"
+var accountCity="bengaluru"
+console.log(accountCity);
+console.table([accountCity,accountEmail,accountPassword]);
