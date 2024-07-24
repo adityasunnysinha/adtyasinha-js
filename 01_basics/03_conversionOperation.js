335466e59f164ad36374890127b2374645d6716a
Aditya Sinha <99471410+adityasunnysinha@users.noreply.github.com>
2024-07-24 20:15:09 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score="33"
+//let score="33abc"
+//let score=true
+console.log(typeof score);
+let  valueInNumber=Number(score);//change string to number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0;
+
+let isScore=1       // true
+//let isScore=0     // false
+//let isScore=""    //false
+//let isScore="g"   //true
+let booleanIsLoggedIn=Boolean(isScore)
+console.log(booleanIsLoggedIn);
+
+//stringNumber => convert number to string
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
