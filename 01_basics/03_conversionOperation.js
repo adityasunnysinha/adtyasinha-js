575903330b657bb92a2762bf91ed65a8b9fdcde0
Aditya Sinha <99471410+adityasunnysinha@users.noreply.github.com>
2024-07-24 21:04:39 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score="33"
 //let score="33abc"
 //let score=true
-console.log(typeof score);
+// console.log(typeof score);
 let  valueInNumber=Number(score);//change string to number
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 //"33"=>33
 //"33abc"=>NaN
 //true=>1; false=>0;
@@ -14,14 +14,67 @@ let isScore=1       // true
 //let isScore=""    //false
 //let isScore="g"   //true
 let booleanIsLoggedIn=Boolean(isScore)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //stringNumber => convert number to string
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//************************Operations************************//
+
+let value=33
+let negValue=-value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(6/2);
+// console.log(2%3);
+
+let str1="hello"
+let str2="   aditya"
+let str3= str1+str2
+// console.log(str3);
+
+//typeconversion automatically convert according to the occurence
+// console.log("1"+2+3);
+// console.log(1+2+"2");
+// console.log((1+2)+"4");
+// console.log(1+2+3);
+// console.log(1+2*3);
+//  console.log(+true);//1
+//  console.log(+"");//0
+
+
+//increament operator
+let gameCounter=100
+gameCounter++;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
+
+
+
 
 
 
