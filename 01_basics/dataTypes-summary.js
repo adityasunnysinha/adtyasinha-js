b1c76f8a3fc50db04671a80c286d00a44ec7dd8e
Aditya Sinha <99471410+adityasunnysinha@users.noreply.github.com>
2024-07-25 13:35:31 +0000

    datatype summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,25 @@
+const id=Symbol('123')
+const anotherId=Symbol('123')
+// console.log(id === anotherId)
+
+const bigNumber=123456789n
+// console.log(typeof bigNumber);//bigint
+
+const nullValue=null
+console.log(typeof null);//object
+
+const heros=["shaktimn","doga"]
+// let myObj{
+//     name:"hitesh",
+//     age:22,
+// }
+
+// const myfunction=function(){
+
+//     console.log("helloworld")
+// }
+
+// console.log(typeof myfunction)// iska datatype function object hoga 
+// phone me kuch screenshot hai 25 jul 24 me dekh lena kon se ka kya datatype aayga
+
+
